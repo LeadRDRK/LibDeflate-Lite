fbc1497c13aec2c5a21018165023723bb0efe236
SafeteeWoW <30888549+SafeteeWoW@users.noreply.github.com>
2019-05-02 21:18:49 +0800

    Travis cleanup

--- .travis/setenv.sh
+++ .travis/setenv.sh
@@ -4,25 +4,15 @@
 # Shouldn't use "set -eufo pipefail"
 # in the script which is sourced
 
-if [ -z "${PLATFORM:-}" ]; then
-  export PLATFORM=$TRAVIS_OS_NAME;
-fi
+export WORKDIR="$(pwd)"
 
-if [ "$PLATFORM" == "osx" ]; then
-  export PLATFORM="macosx";
-fi
+echo "Working directory: ${WORKDIR}"
 
-if [ -z "$PLATFORM" ]; then
-  if [ "$(uname)" == "Linux" ]; then
-    export PLATFORM="linux";
-  else
-    export PLATFORM="macosx";
-  fi;
-fi
+echo "PLATFORM: ${PLATFORM}"  # "Linux" for linux, "Darwin" for macosx
 
 mkdir -p $HOME/install
 mkdir -p $HOME/.lua
-export PATH=${PATH}:${TRAVIS_BUILD_DIR}/tests:$HOME/.lua:$HOME/.local/bin:${HOME}/install/luarocks/bin
+export PATH=${PATH}:$HOME/.lua:$HOME/.local/bin:${HOME}/install/luarocks/bin
 export LUA_SRC_DIR=$HOME/luasrc
 export LUA_HOME_DIR=$HOME/install/$LUA
 export LR_HOME_DIR=$HOME/install/luarocks
@@ -30,5 +20,3 @@ export LR_HOME_DIR=$HOME/install/luarocks
 mkdir -p "${LUA_SRC_DIR}"
 mkdir -p "${LUA_HOME_DIR}"
 mkdir -p "${LR_HOME_DIR}"
-
-hash -r
